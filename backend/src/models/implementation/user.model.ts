fa62be7e1bda96e2384c358775ac378cd0e2bb56
Nadeem-pc <nadeempc777@gmail.com>
2025-08-12 16:14:02 +0530

    feat: add status field to user model

--- backend/src/models/implementation/user.model.ts
+++ backend/src/models/implementation/user.model.ts
@@ -1,4 +1,3 @@
-import { hashPassword } from "@/utils/bcrypt.util";
 import { model, Schema } from "mongoose";
 import { IUserModel } from "../interface/user.model.interface";
 
@@ -19,30 +18,17 @@ const userSchema = new Schema<IUserModel>({
     password: {
         type: String
     },
-    // status: {
-    //     type: String, 
-    //     enum: ["Active", "Blocked"],
-    //     default: "Active"
-    // },
-    // role: {
-    //     type: String, 
-    //     enum: ["User", "Therapist"],
-    //     default: "User"
-    // },
-    // profilePicture: {
-    //     type: String
-    // },
-    // dateOfBirth: {
-    //     type: Date,
-    // },
+    role: {
+        type: String, 
+        enum: ["client", "therapist", "admin"],
+        default: "client"
+    },
+    status: {
+        type: String, 
+        enum: ["Active", "Blocked"],
+        default: "Active"
+    },
 });
-
-// userSchema.pre<IUserModel>("save", async function (next) {
-//     if(this.isModified("password")) {
-//         this.password = await hashPassword(this.password)
-//     }
-//     next()
-// })
 
 const User = model<IUserModel>("User", userSchema);
 export default User;
