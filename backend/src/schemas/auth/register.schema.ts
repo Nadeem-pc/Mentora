a75edd23aa98f9ffdd48463590de7473d07fca96
Nadeem-pc <nadeempc777@gmail.com>
2025-08-20 10:20:50 +0530

    style(auth): format register schema for readability

--- backend/src/schemas/auth/register.schema.ts
+++ backend/src/schemas/auth/register.schema.ts
@@ -1,19 +1,18 @@
-import { HttpResponse } from "../../constants/response-message.constant";
 import { z } from "zod";
+import { HttpResponse } from "../../constants/response-message.constant";
 
 const nameSchema = z
-.string()
-.min(3, "Must be at least 3 characters")
-.max(20, "Must be at most 20 characters")
-.regex(/^[A-Za-z]+$/, "Must contain only letters with no spaces");
+  .string()
+  .min(3, "Must be at least 3 characters")
+  .max(20, "Must be at most 20 characters")
+  .regex(/^[A-Za-z]+$/, "Must contain only letters with no spaces");
 
-export const registerSchema = z
-  .object({
-    firstName: nameSchema,
-    lastName: nameSchema,
-    role: z.string(),
-    email: z.string().email(HttpResponse.INVALID_EMAIL),
-    password: z
+export const registerSchema = z.object({
+  firstName: nameSchema,
+  lastName: nameSchema,
+  role: z.string(),
+  email: z.string().email(HttpResponse.INVALID_EMAIL),
+  password: z
     .string()
     .min(8, "Password must be at least 8 characters long")
     .regex(/[a-z]/, "Password must contain at least one lowercase letter")
