f08ef5a595f29e4a29e1f02617ee342135d970d9
Nadeem-pc <nadeempc777@gmail.com>
2025-09-06 21:15:03 +0530

    feat(auth): extend IAuthController with refreshAccessToken, login, forgotPassword and resetPassword

--- backend/src/controllers/interface/IAuthController.ts
+++ backend/src/controllers/interface/IAuthController.ts
@@ -4,4 +4,10 @@ export interface IAuthController {
     registerUser(req: Request, res: Response, next: NextFunction): Promise<void>;
     verifyOtp(req: Request, res: Response, next: NextFunction): Promise<void>;
     resendOtp(req: Request, res: Response, next: NextFunction): Promise<void>;
+    refreshAccessToken(req: Request, res: Response, next: NextFunction): Promise<void>;
+    login(req: Request, res: Response, next: NextFunction): Promise<void>;
+    forgotPassword(req: Request, res: Response, next: NextFunction): Promise<void>;
+    resetPassword(req: Request, res: Response, next: NextFunction): Promise<void>;
+    
+    // me(req: Request, res: Response, next: NextFunction): Promise<void>;
 };
