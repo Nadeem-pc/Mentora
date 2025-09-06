838b260bc14ec1c954acc3f66660ab30ec84abcd
Nadeem-pc <nadeempc777@gmail.com>
2025-09-06 21:21:35 +0530

    feat(base-repo): add findById and findAll methods to BaseRepository

--- backend/src/repositories/base.repository.ts
+++ backend/src/repositories/base.repository.ts
@@ -2,10 +2,15 @@ import {
     Document,
     FilterQuery,
     Model,
+    Types,
 } from "mongoose";
 
 export abstract class BaseRepository<T extends Document> {
     constructor(protected model: Model<T>) {}
+
+    async findById(id: Types.ObjectId): Promise<T | null> {
+        return this.model.findById(id);
+    }
 
     async create(data: Partial<T>): Promise<T> {
         const document = new this.model(data);
@@ -14,5 +19,9 @@ export abstract class BaseRepository<T extends Document> {
 
     async findOne(filter: FilterQuery<T>): Promise<T | null>{
         return this.model.findOne(filter);
+    }
+
+    async findAll(): Promise<T[]> {
+        return this.model.find()
     }
 };
