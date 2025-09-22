ff7871255c5e059c6c2568f2ebb65acf7d3de296
Nadeem-pc <nadeempc777@gmail.com>
2025-09-22 20:41:28 +0530

    refactor(middleware): replace role-based verification.middleware with generic verifyToken middleware

--- backend/src/middlewares/verify-token.middleware.ts
+++ backend/src/middlewares/verify-token.middleware.ts
@@ -1,50 +1,30 @@
 import { Request, Response, NextFunction } from "express";
 import { verifyAccessToken } from "@/utils/jwt.util";
-import {HttpStatus} from "@/constants/status.constant";
+import { HttpStatus } from "@/constants/status.constant";
 import { createHttpError } from "@/utils/http-error.util";
-import { HttpResponse } from "../constants/response-message.constant";
+import { HttpResponse } from "@/constants/response-message.constant";
 
-export default function (
-    userLevel: "User" | "Admin" | "Therapist"
-): (req: Request, res: Response, next: NextFunction) => void {
+export default function verifyToken() {
     return (req: Request, _res: Response, next: NextFunction): void => {
         const authHeader = req.headers.authorization;
 
-        console.log("Authorization Header:", authHeader);
-
-        if (!authHeader || !authHeader.startsWith("Bearer")) {
-            throw createHttpError(HttpStatus.UNAUTHORIZED, HttpResponse.NO_TOKEN)
+        if (!authHeader || !authHeader.startsWith("Bearer ")) {
+            throw createHttpError(HttpStatus.UNAUTHORIZED, HttpResponse.NO_TOKEN);
         }
 
         const token = authHeader.split(" ")[1];
-        if (!token) {
-            throw createHttpError(HttpStatus.UNAUTHORIZED, HttpResponse.NO_TOKEN)
-        }
-
         const payload = verifyAccessToken(token) as {
             id: string;
             email: string;
-            role: "User" | "Admin" | "Therapist"
+            role: "client" | "admin" | "therapist";
         };
 
         if (!payload) {
-            console.log("Invalid token payload");
-            throw createHttpError(HttpStatus.UNAUTHORIZED, HttpResponse.TOKEN_EXPIRED)
+            throw createHttpError(HttpStatus.UNAUTHORIZED, HttpResponse.TOKEN_EXPIRED);
         }
 
-        if (payload.role !== userLevel) {
-            throw createHttpError(HttpStatus.UNAUTHORIZED, HttpResponse.UNAUTHORIZED)
-        }
+        (req as any).user = payload;
 
-        req.user = {
-            id: payload.id,
-            email: payload.email,
-            role: payload.role,
-        }
-
-        console.log("User payload:", req.user);
-
-        req.headers["x-user-payload"] = JSON.stringify(payload);
         next();
     };
-}
+};
