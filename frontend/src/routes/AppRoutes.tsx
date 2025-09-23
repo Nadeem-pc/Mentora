2b4f635834fb017f55c5c61453edc2dd73cd915a
Nadeem-pc <nadeempc777@gmail.com>
2025-09-23 10:45:05 +0530

    feat(routes): update AppRoutes.tsx with admin, therapist, and additional client routes

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -3,25 +3,71 @@ import PreAuth from "@/pages/auth/PreAuth";
 import AuthLayout from "@/layouts/AuthLayout";
 import Auth from "@/pages/auth";
 import Landing from "@/pages/client/Landing";
-import OtpForm from "@/components/auth/OTPForm";
+import UnProtectedRoute from "./UnProtectedRoute";
+import OtpForm from "@/components/auth/OtpForm";
+import AdminLayout from "@/layouts/AdminLayout";
+import ClientManagement from "@/pages/admin/ClientManagement";
+import ForgotPassword from "@/components/auth/ForgotPassword";
+import ResetPassword from "@/components/auth/ResetPassword";
+import UserProfilePage from "@/pages/client/Profile";
+import NotFoundPage from "@/pages/shared/PageNotFound";
+import UserDetail from "@/pages/admin/UserDetail";
+import TherapistProfilePage from "@/pages/therapist/Profile";
+import TherapistDashboard from "@/pages/therapist/Dashboard";
+import ProtectedRoute from "./ProtectedRoute";
+import Dashboard from "@/pages/admin/Dashboard";
 
 export const router = createBrowserRouter([
     {
-        path: '/auth-layout', element: <AuthLayout/>, 
-        
+        path: '/', element: <Landing/> 
+    },
+    {
+        path: '/auth', element: <AuthLayout/>, 
         children: [
             {
-                path: 'pre', element: <PreAuth/>
+                path: 'role', element: <PreAuth/>
             },
             {
-                path: 'auth', element: <Auth/>
+                path: 'form', element: <UnProtectedRoute> <Auth/> </UnProtectedRoute>
             },
             {
-                path: 'verify-otp', element: <OtpForm/>
+                path: 'verify-otp', element: <UnProtectedRoute> <OtpForm/> </UnProtectedRoute>
+            },
+            {
+                path: 'forgot-password', element: <ForgotPassword/>
+            },
+            {
+                path: 'reset-password', element: <ResetPassword />
             },
         ]
     },
     {
-        path: '/', element: <Landing/>
-    }
-])
+        path: '/profile',  element: <ProtectedRoute allowedRoles={['client']}> <UserProfilePage /> </ProtectedRoute>
+    },
+
+    {
+        path: '/admin',  element: <ProtectedRoute allowedRoles={['admin']}> <AdminLayout /> </ProtectedRoute>,
+        children: [
+            {
+                path: 'dashboard', element: <Dashboard/>
+            },
+            {
+                path: 'users', element: <ClientManagement/>
+            },
+            {
+                path: 'users/detail', element: <UserDetail/>
+            }
+        ]
+    },
+
+    {
+        path: '/therapist/profile', element: <ProtectedRoute allowedRoles={['therapist']}> <TherapistProfilePage/> </ProtectedRoute>
+    },
+    {
+        path: '/therapist/dashboard', element: <ProtectedRoute allowedRoles={['therapist']}> <TherapistDashboard/> </ProtectedRoute>
+    },
+
+    {
+        path: '*', element: <NotFoundPage/>
+    },
+]);
