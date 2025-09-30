749360aaa0fdc8f42ee8b48cb304fc7da73e1381
Nadeem-pc <nadeempc777@gmail.com>
2025-09-30 19:45:25 +0530

    feat(models): add therapist model

---
+++ backend/src/models/implementation/therapist.model.ts
@@ -0,0 +1,45 @@
+import { Schema } from "mongoose";
+import { ITherapistModel } from "../interface/therapist.model.interface";
+import User from "./user.model";
+
+const therapistSchema = new Schema<ITherapistModel>({
+    experience: {
+        type: String,
+        default: null
+    },
+    fee: {
+        type: String,
+        default: null
+    },
+    qualification: {
+        type: String,
+        default: null
+    },
+    specializations: {
+        type: [String],
+        default: []
+    },
+    languages: {
+        type: [String],
+        default: []
+    },
+    about: {
+        type: String,
+        default: null
+    },
+    resume: {
+        type: String,
+        default: null
+    },
+    certifications: {
+        type: [String],
+        default: []
+    },
+    approvalStatus: {
+        type: String,
+        enum: ['Pending', 'Requested', 'Approved', 'Rejected'],
+        default: 'Pending'
+    }
+}, { timestamps: true });
+
+export const TherapistDetails = User.discriminator<ITherapistModel>("therapist", therapistSchema);
