6dbe3945e9afc46ad74ee22ca2a38a8c509e263f
Nadeem-pc <nadeempc777@gmail.com>
2025-10-06 15:55:42 +0530

    feat(models): added wallet model

---
+++ backend/src/models/implementation/wallet.model.ts
@@ -0,0 +1,23 @@
+import { model, Schema } from "mongoose";
+import { IWalletModel } from "../interface/wallet.model.interface";
+
+const walletSchema = new Schema<IWalletModel>({
+    ownerId: {
+        type: Schema.Types.ObjectId,
+        required: true,
+    },
+    ownerType: {
+        type: String,
+        enum: ["client", "therapist", "admin"],
+        required: true, 
+    },
+    balance: {
+        type: Number,
+        required: true,
+        default: 0,
+        min: 0,
+    }
+}, { timestamps: true });
+
+export const Wallet = model<IWalletModel>("Wallet", walletSchema);
+export default Wallet;
