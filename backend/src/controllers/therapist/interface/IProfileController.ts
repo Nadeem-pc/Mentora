41d521d1735be37cbd4152bbc48fb18352b37917
Nadeem-pc <nadeempc777@gmail.com>
2025-10-08 10:21:22 +0530

    feat(therapist): add interface for TherapistProfileController

---
+++ backend/src/controllers/therapist/interface/IProfileController.ts
@@ -0,0 +1,8 @@
+import { NextFunction, Request, Response } from "express";
+
+export interface ITherapistProfileController {
+    getTherapistProfile(req: Request, res: Response, next: NextFunction): Promise<void>;
+    updateProfile(req: Request, res: Response, next: NextFunction): Promise<void>;
+    preSignedURL(req: Request, res: Response, next: NextFunction): Promise<void>;
+    getPreSignedURL(req: Request, res: Response, next: NextFunction): Promise<void>;
+};
