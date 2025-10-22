e2d441f2b51fe1425f44b0b2edfaab5c0b088819
Nadeem-pc <nadeempc777@gmail.com>
2025-10-22 09:46:03 +0530

    chore(env): add Stripe publishable key and Google client ID to env config

--- frontend/src/config/env.config.ts
+++ frontend/src/config/env.config.ts
@@ -1,3 +1,5 @@
 export const env = {
     BACKEND_URL: import.meta.env.VITE_BASE_URL,
-}
+    STRIPE_PUBLISHABLE_KEY: import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY,
+    GOOGLE_CLIENT_ID: import.meta.env.VITE_GOOGLE_CLIENT_ID,
+};
