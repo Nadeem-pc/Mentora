2c573b8c9a702343bac2ec01b89ca91af6040f0a
Nadeem-pc <nadeempc777@gmail.com>
2025-11-07 14:35:24 +0530

    feat(router): add appointment routes and apply verifyToken middleware to therapist router

--- backend/src/routes/therapist/index.router.ts
+++ backend/src/routes/therapist/index.router.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import therapistProfileRouter from "./profile.router";
 import slotRouter from "./slot.router";
+import appointmentRouter from "./appointments.router";
+import verifyToken from "@/middlewares/verify-token.middleware";
 
 const therapistRouter = Router();
+therapistRouter.use(verifyToken());
 
 therapistRouter.use('/', therapistProfileRouter);
 therapistRouter.use('/', slotRouter);
+therapistRouter.use('/', appointmentRouter);
 
 export default therapistRouter;
