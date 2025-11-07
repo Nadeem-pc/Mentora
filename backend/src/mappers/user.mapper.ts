673115187b85f3414d6d93b64a3da6c39a35d449
Nadeem-pc <nadeempc777@gmail.com>
2025-11-07 14:04:59 +0530

    refactor(mapper): implement profile DTO mapping methods in user.mapper.ts

--- backend/src/mappers/user.mapper.ts
+++ backend/src/mappers/user.mapper.ts
@@ -1,10 +1,9 @@
 import { IUserModel } from "@/models/interface/user.model.interface";
-import { IUserDTO, IUserListDTO } from "@/dtos/user.dto";
+import { IUserDTO, IUserListDTO, IUserProfileDTO } from "@/dtos/user.dto";
 
 export class UserMapper {
   static toDTO(user: IUserModel): IUserDTO {
     return {
-      _id: user._id.toString(),
       firstName: user.firstName,
       lastName: user.lastName,
       email: user.email,
@@ -30,11 +29,28 @@ export class UserMapper {
     };
   }
 
+  static toProfileDTO(user: IUserModel): IUserProfileDTO {
+    return {
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      phone: user.phone || null,
+      gender: user.gender || null,
+      dob: user.dob || null,
+      createdAt: user.createdAt,
+      profileImg: user.profileImg || null
+    };
+  }
+
   static toDTOs(users: IUserModel[]): IUserDTO[] {
     return users.map((user) => this.toDTO(user));
   }
 
   static toListDTOs(users: IUserModel[]): IUserListDTO[] {
     return users.map((user) => this.toListDTO(user));
+  }
+
+  static toProfileDTOs(users: IUserModel[]): IUserProfileDTO[] {
+    return users.map((user) => this.toProfileDTO(user));
   }
 }
