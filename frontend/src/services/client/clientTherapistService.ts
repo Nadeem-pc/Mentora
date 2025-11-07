9e4d38ed6d30f3825129a2d37360e64a6a584b70
Nadeem-pc <nadeempc777@gmail.com>
2025-11-07 15:17:40 +0530

    feat(clientTherapistService): add methods to fetch therapists and their schedules

---
+++ frontend/src/services/client/clientTherapistService.ts
@@ -0,0 +1,46 @@
+import { axiosInstance } from "@/config/axios.config";
+import { API } from "@/constants/api.constant";
+
+export const clientTherapistService = {
+    getTherapists: async () => {
+        try {
+            const response = await axiosInstance.get(API.CLIENT.GET_THERAPISTS_LIST);
+            return response.data;
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    },
+
+    getTherapistDetails: async (therapistId: string) => {
+        try {
+            const response = await axiosInstance.get(API.CLIENT.GET_THERAPIST_DETAILS(therapistId));
+            return response.data;
+        } catch (error){
+            console.error(error);
+            throw error;
+        }
+    },
+
+    getTherapistWeeklySchedule: async (therapistId: string) => {
+        try {
+            const response = await axiosInstance.get(API.CLIENT.GET_THERAPIST_SLOTS(therapistId));
+            return response.data;
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    },
+
+    getAvailableSlotsForDate: async (therapistId: string, date: string) => {
+        try {
+            const response = await axiosInstance.get(
+                API.CLIENT.GET_AVAILABLE_SLOTS_FOR_DATE(therapistId, date)
+            );
+            return response.data;
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    }
+};
