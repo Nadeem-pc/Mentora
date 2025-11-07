8c33882911ddffc53e28ae78a4e594bae373309e
Nadeem-pc <nadeempc777@gmail.com>
2025-11-07 13:45:52 +0530

    refactor(slot): update ISlotController interface

--- backend/src/controllers/therapist/interface/ISlotController.ts
+++ backend/src/controllers/therapist/interface/ISlotController.ts
@@ -1,7 +1,7 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 
 export interface ISlotController {
-    createSlot(req: Request, res: Response, next: NextFunction): Promise<void>;
-    getSlots(req: Request, res: Response, next: NextFunction): Promise<void>;
-    deleteSlot(req: Request, res: Response, next: NextFunction): Promise<void>;
-};
+    createWeeklySchedule(req: Request, res: Response, next: NextFunction): Promise<void>;
+    getWeeklySchedule(req: Request, res: Response, next: NextFunction): Promise<void>;
+    updateWeeklySchedule(req: Request, res: Response, next: NextFunction): Promise<void>;
+}
